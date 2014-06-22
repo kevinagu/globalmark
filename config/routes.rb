83c4a2de94b56564d2e60f1bce75a8c210c13ac7
kevin agudelo <kevinagu90@gmail.com>
2014-06-22 12:14:55 -0500

    devise configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :productos
 
   resources :servicios
 
   resources :users
+
+root to: "servicios#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
